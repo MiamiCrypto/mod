bf470dde916957cb8c7dddc6577d00cdbf148b77
Miami Crypto Labs <105951495+MiamiCrypto@users.noreply.github.com>
2024-01-19 21:23:24 -0500

    app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ api_key = st.secrets["openai_secret"]
 client = OpenAI(api_key=api_key)
 
 # Streamlit UI components
-st.title('''Natural Language Processing Detection''')
+st.title('''Natural Language Processing Detection (sample)''')
 
 user_input = st.text_area("Enter text to moderate")
 
